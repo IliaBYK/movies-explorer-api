3e273c8ce57a154f70d31c35ee32d5d4b4fc9368
Ilia Bykov <fagotist96@icloud.com>
2023-02-26 13:07:53 +0300

    host

--- src/app.js
+++ src/app.js
@@ -9,37 +9,23 @@ import limiter from './middlewares/limiter.js';
 import unknownErrorHandler from './errorHandlers/unknownErrorHandler.js';
 import errorsHandler from './errorHandlers/errorsHandler.js';
 import router from './routes/index.router.js';
-import { INTERNAL_SERVER_ERR_CODE } from './utils/errorsCodes.js';
-import { INTERNAL_MESSAGE } from './utils/constants.js';
 import config from './utils/config.js';
 
 const app = express();
 
-async function startApp() {
-  try {
-    set('strictQuery', false);
-    await connect(config.BASE_PATH);
-    app.use(urlencoded({ extended: true }));
-    app.use(cookieParser());
-    app.use(requestLogger);
-    app.use(logerErrors);
-    app.use(limiter);
-    app.use(json());
-    app.use(helmet());
-    app.use('/', router);
-    app.use(celebrateErrors());
-    app.use(errorsHandler);
-    app.use(unknownErrorHandler);
-  } catch (err) {
-    if (err) {
-      app.use((req, res) => {
-        res.status(INTERNAL_SERVER_ERR_CODE).send({ message: INTERNAL_MESSAGE });
-      });
-    }
-  }
-}
-
-startApp();
+set('strictQuery', false);
+await connect(config.BASE_PATH);
+app.use(urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(requestLogger);
+app.use(logerErrors);
+app.use(limiter);
+app.use(json());
+app.use(helmet());
+app.use('/', router);
+app.use(celebrateErrors());
+app.use(errorsHandler);
+app.use(unknownErrorHandler);
 
 app.listen(config.PORT, () => {
   log(`App has been started on port ${config.PORT}...`);
