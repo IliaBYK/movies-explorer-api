b02aa6f5f35bb50ccce2e340cd0f3d8c0626e817
Ilia Bykov <fagotist96@icloud.com>
2023-02-26 12:56:40 +0300

    host

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 4000,
   JWT_KEY: process.env.JWT_KEY || 'jwtmoviesearchingauthorization',
-  BASE_PATH: process.env.BASE_PATH || 'mongodb://localhost:27017/moviesearchdb',
+  BASE_PATH: process.env.BASE_PATH || 'mongodb://localhost:27017/bitfilmsdb',
 };
