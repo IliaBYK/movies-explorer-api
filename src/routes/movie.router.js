79528151ee207ae434fd20fdbd637adfa66522c8
Ilia Bykov <fagotist96@icloud.com>
2023-03-23 16:22:10 +0300

    id validation

--- src/routes/movie.router.js
+++ src/routes/movie.router.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { getMovies, postMovie, deleteMovie } from '../controllers/movieController.js';
-import { movieCreateValidation } from '../middlewares/validation.js';
+import { movieCreateValidation, idValidation } from '../middlewares/validation.js';
 
 const router = Router();
 
 router.get('', getMovies);
 router.post('', movieCreateValidation, postMovie);
-router.delete('/:id', deleteMovie);
+router.delete('/:id', idValidation, deleteMovie);
 
 export default router;
