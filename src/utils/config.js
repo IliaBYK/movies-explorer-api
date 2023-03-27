4359793c88a02198a6e7595da0c86b3b20594ac2
Ilia Bykov <fagotist96@icloud.com>
2023-03-28 02:54:01 +0300

    cors

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,5 +5,5 @@ config();
 export default {
   PORT: process.env.PORT || 4000,
   JWT_KEY: process.env.JWT_KEY || 'jwtmoviesearchingauthorization',
-  BASE_PATH: process.env.BASE_PATH || 'mongodb://127.0.0.1:27017/bitfilmsdb',
+  BASE_PATH: process.env.BASE_PATH || 'wss://127.0.0.1:27017/bitfilmsdb',
 };
