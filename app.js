507cf3e5658600b2e37cc68ba6c8d67458893f4b
Ilia Bykov <fagotist96@icloud.com>
2023-03-28 02:46:59 +0300

    cors

--- app.js
+++ app.js
@@ -7,13 +7,29 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { requestLogger, logerErrors } from './src/middlewares/logger.js';
 import limiter from './src/middlewares/limiter.js';
-import corsOptionsDelegate from './src/middlewares/cors.js';
 import unknownErrorHandler from './src/errorHandlers/unknownErrorHandler.js';
 import errorsHandler from './src/errorHandlers/errorsHandler.js';
 import router from './src/routes/index.router.js';
 import config from './src/utils/config.js';
 
 const app = express();
+
+const whitelist = [
+  'http://localhost:3000',
+  'https://localhost:3000',
+  'http://bitfilms.ibyk.nomoredomainsclub.ru',
+  'https://bitfilms.ibyk.nomoredomainsclub.ru',
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+};
 
 set('strictQuery', false);
 
@@ -24,7 +40,7 @@ app.use(requestLogger);
 app.use(limiter);
 app.use(helmet());
 app.use(json());
-app.use(cors(corsOptionsDelegate));
+app.use(cors(corsOptions));
 app.use('/', router);
 app.use(logerErrors);
 app.use(celebrateErrors());
