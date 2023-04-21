a0498f6398134decdc5b0ba59e26d5f8660d0826
Ilia Bykov <fagotist96@icloud.com>
2023-04-21 19:25:52 +0300

    deleteMovie: owner

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -33,8 +33,8 @@ export async function deleteMovie(req, res, next) {
     if (movie.owner._id.toString() !== req.user._id) {
       throw new ForbiddenError(FORBIDDEN_MESSAGE);
     }
-    await movie.delete();
     res.send(movie.owner._id.toString(), req.user._id);
+    await movie.delete();
   } catch (err) {
     next(err);
   }
