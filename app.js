364a3ce8972d1e89aecbf08ee78dd8e9d29c2efa
Ilia Bykov <fagotist96@icloud.com>
2023-04-26 14:47:15 +0300

    cors

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ import { log } from 'console';
 import helmet from 'helmet';
 import { errors as celebrateErrors } from 'celebrate';
 import cookieParser from 'cookie-parser';
-/* import cors from 'cors'; */
-import cors from './src/middlewares/cors.js';
+import cors from 'cors';
 import { requestLogger, logerErrors } from './src/middlewares/logger.js';
 import limiter from './src/middlewares/limiter.js';
 import unknownErrorHandler from './src/errorHandlers/unknownErrorHandler.js';
@@ -15,7 +14,7 @@ import config from './src/utils/config.js';
 
 const app = express();
 
-/* const whitelist = [
+const whitelist = [
   'http://localhost:3000',
   'http://bitfilms.ibyk.nomoredomainsclub.ru',
   'https://bitfilms.ibyk.nomoredomainsclub.ru',
@@ -29,7 +28,7 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
-}; */
+};
 
 set('strictQuery', false);
 
@@ -40,7 +39,7 @@ app.use(cookieParser());
 app.use(requestLogger);
 app.use(limiter);
 app.use(helmet());
-app.use(cors);
+app.use(cors(corsOptions));
 app.use('/', router);
 app.use(logerErrors);
 app.use(celebrateErrors());
